bc8922bb26a63e2e97a8bf7368b342e15cf48245
kimwwweee <cheonghak.t230206@ggh.goe.go.kr>
2025-05-13 11:38:32 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dd",
+      "name": "dd",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 360,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0035618964326431524,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-download"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-download",
+      "name": "download",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 79.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2173727901881808,
+          "pitch": 0.3313551953532663,
+          "rotation": 0,
+          "target": "0-dd"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
